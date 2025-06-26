4ef382e774acf9088b2a36bfbf28232c0c2cf7d2
shiranshalom <shiran@ravendb.net>
2025-06-26 10:44:57 +0300

    RavenDB-24347 - Orleans - Fix GitHub Actions tests

---
+++ tests/UnitTests/RavenDbGrainPersistenceTestsRunner.cs
@@ -0,0 +1,12 @@
+﻿using TestExtensions;
+using TestExtensions.Runners;
+using Xunit.Abstractions;
+
+namespace UnitTests
+{
+    public class RavenDbGrainPersistenceTestsRunner(
+        ITestOutputHelper output,
+        BaseTestClusterFixture fixture,
+        string grainNamespace = "UnitTests.Grains")
+        : GrainPersistenceTestsRunner(output, fixture, grainNamespace);
+}
