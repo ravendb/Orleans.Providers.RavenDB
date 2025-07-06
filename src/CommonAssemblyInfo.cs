fa797f1408557e379972443a8bb702c928476691
aviv <aviv@ravendb.net>
2025-07-06 14:18:37 +0300

    RavenDB-24347 : bump version

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,13 +1,13 @@
-﻿﻿using System;
+﻿using System;
 using System.Diagnostics;
 using System.Reflection;
 using System.Resources;
 
 [assembly: AssemblyCopyright("© Hibernating Rhinos 2009 - 2025 All rights reserved.")]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-75f90d1")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1-d1205f7")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
