6a913b278aac95f679a03bce4dab2443d685176f
aviv <aviv@ravendb.net>
2025-08-11 16:39:09 +0300

    RavenDB-24223 : add gateway list provider builder for auto-detection of RavenDB gateway list via Aspire

---
+++ src/Orleans.Providers.RavenDB/Hosting/RavenDbGatewayListProviderBuilder .cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Orleans.Messaging;
+using Orleans.Providers.RavenDb.Configuration;
+using Orleans.Providers.RavenDb.Membership;
+
+[assembly: RegisterProvider("RavenDbGateway", "GatewayListProvider", "Client", typeof(Orleans.Providers.RavenDb.Hosting.RavenDbGatewayListProviderBuilder))]
+
+namespace Orleans.Providers.RavenDb.Hosting;
+
+internal sealed class RavenDbGatewayListProviderBuilder : IProviderBuilder<IClientBuilder>
+{
+    public void Configure(IClientBuilder builder, string name, IConfigurationSection section)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.Configure<RavenDbMembershipOptions>(section);
+            services.AddSingleton<IGatewayListProvider, RavenDbGatewayListProvider>();
+        });
+    }
+}
