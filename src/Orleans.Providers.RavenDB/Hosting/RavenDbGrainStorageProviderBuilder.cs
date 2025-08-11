13377c8b89ba66196e306ca9524a5ae844d6ebdd
aviv <aviv@ravendb.net>
2025-08-11 14:59:12 +0300

    RavenDB-24223: Add grain storage provider builder for automatic registration with Orleans Aspire

---
+++ src/Orleans.Providers.RavenDB/Hosting/RavenDbGrainStorageProviderBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Orleans.Providers.RavenDb.StorageProviders;
+
+[assembly: RegisterProvider("RavenDbGrainStorage", "GrainStorage", "Silo", typeof(Orleans.Providers.RavenDb.Hosting.RavenDbGrainStorageProviderBuilder))]
+
+namespace Orleans.Providers.RavenDb.Hosting
+{
+    internal sealed class RavenDbGrainStorageProviderBuilder : IProviderBuilder<ISiloBuilder>
+    {
+        public void Configure(ISiloBuilder builder, string name, IConfigurationSection section)
+        {
+            builder.AddRavenDbGrainStorage(name, section.Bind);
+        }
+    }
+}
