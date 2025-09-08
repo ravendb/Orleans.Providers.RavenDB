db9a7aeb1bcccd3a41b2cff289e0b477ca7cf7ef
aviv <aviv@ravendb.net>
2025-09-08 12:25:08 +0300

    RavenDB-24868 : bump version

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Resources;
 
 [assembly: AssemblyCopyright("© Hibernating Rhinos 2009 - 2025 All rights reserved.")]
 
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
-[assembly: AssemblyInformationalVersion("1.0.2-d8963b0")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyInformationalVersion("1.0.3-193bb9e")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
