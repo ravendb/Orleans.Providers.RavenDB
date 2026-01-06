d61a5da1b64de784d9aab624a0d2aaeec9793f74
aviv <aviv@ravendb.net>
2026-01-06 18:46:32 +0200

    RavenDB-25646 : bump version to 1.0.6

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Resources;
 
 [assembly: AssemblyCopyright("© Hibernating Rhinos 2009 - 2025 All rights reserved.")]
 
-[assembly: AssemblyVersion("1.0.5")]
-[assembly: AssemblyFileVersion("1.0.5")]
-[assembly: AssemblyInformationalVersion("1.0.5-fd00f57")]
+[assembly: AssemblyVersion("1.0.6")]
+[assembly: AssemblyFileVersion("1.0.6")]
+[assembly: AssemblyInformationalVersion("1.0.6-175f97f")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
